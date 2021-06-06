a3bf9f6fb468404c2a9cec8eeef9030b1732452d
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2021-06-07 02:52:14 +0430

    start to get from pluf

---
+++ src/api/shop-pluf.js
@@ -0,0 +1,94 @@
+/**
+ * Mocking client-server processing
+ */
+
+
+//{
+//		"id": 1,
+//		"name": "Flare Dress",
+//		"price": 120,
+//		"salePrice": 200,
+//		"discount": 50,
+//		"pictures": [
+//			"https://dummyimage.com/736x1000/cccccc/0011ff.png",
+//			"https://dummyimage.com/736x1000/cccccc/0011ff.png",
+//			"https://dummyimage.com/736x1000/cccccc/0011ff.png",
+//			"https://dummyimage.com/736x1000/cccccc/0011ff.png"
+//		],
+//		"shortDetails": "Sasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem,",
+//		"description": "Lorem versions of Lorem Ipsum.",
+//		"stock": 16,
+//		"new": true,
+//		"sale": true,
+//		"category": "women",
+//		"colors": [
+//			"yellow",
+//			"gray",
+//			"green"
+//		],
+//		"size": [
+//			"M",
+//			"L",
+//			"XL"
+//		],
+//		"tags": [
+//			"nike",
+//			"caprese"
+//		],
+//		"rating": 4,
+//		"variants": [
+//			{
+//				"color": "yellow",
+//				"images": "https://dummyimage.com/736x1000/cccccc/0011ff.png"
+//			},
+//			{
+//				"color": "gray",
+//				"images": "https://dummyimage.com/736x1000/cccccc/0011ff.png"
+//			},
+//			{
+//				"color": "green",
+//				"images": "https://dummyimage.com/736x1000/cccccc/0011ff.png"
+//			}
+//		]
+//	},
+
+const TIMEOUT = 100
+
+
+function encodeQueryParams(params) {
+	// this line takes the params object and builds the query string
+	const esc = encodeURIComponent;
+	return Object.keys(params).map(k => `${esc(k)}=${esc(params[k])}`).join('&')
+}
+
+function mapProducts(products) {
+
+}
+
+function getProducts(cb) {
+	// encode to scape spaces
+	const url = '/api/v2/shop/products?';
+	const params = {
+		graphql: '{' +
+			'id,title,description,brand,model,manufacturer,avatar,price,off,' +
+			'categories{id, name},' +
+			'metafields{id, key, namespace, product_id, unit, value}' +
+			'}',
+	};
+
+
+	fetch(url + encodeQueryParams(params))
+		.then(response => response.json())
+		.then(data => mapProducts(data.items))
+		.then(products => cb(products));
+}
+
+function buyProducts(payload, cb, timeout) {
+	return setTimeout(() => cb(), timeout || TIMEOUT);
+}
+
+export default {
+	getProducts: getProducts,
+	buyProducts: buyProducts
+};
+
