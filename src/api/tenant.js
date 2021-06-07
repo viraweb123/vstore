04d9d2496c76d2addf14d4f3a7f1cad8810e80e6
hadi2 <hadi2@localhost>
2021-06-08 04:26:13 +0430

    !

---
+++ src/api/tenant.js
@@ -0,0 +1,16 @@
+
+export function getSlides() {
+	return [{
+		title: 'men fashion',
+		subtitle: 'welcome to fashion',
+		link: '/shop',
+		linkTitle: 'shop now',
+		image: 'https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'
+	}, {
+		title: 'women fashion',
+		subtitle: 'welcome to fashion',
+		link: '/shop',
+		linkTitle: 'shop now',
+		image: 'https://images.ctfassets.net/hrltx12pl8hq/oZL5bg7l1t7ZvXxpe6OBe/337244daf1cb2f4420806a40aee81df1/shutterstock_image__9_-min.jpg?fit=fill&w=800&h=300'
+	}];
+};
