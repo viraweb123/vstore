1ef28de8323c83f40961a56c5dbb894bfc773f97
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2021-06-07 05:44:50 +0430

    fetch products

--- src/api/shop-pluf.js
+++ src/api/shop-pluf.js
@@ -4,53 +4,15 @@
 
 
 //{
-//		"id": 1,
-//		"name": "Flare Dress",
-//		"price": 120,
-//		"salePrice": 200,
-//		"discount": 50,
-//		"pictures": [
-//			"https://dummyimage.com/736x1000/cccccc/0011ff.png",
-//			"https://dummyimage.com/736x1000/cccccc/0011ff.png",
-//			"https://dummyimage.com/736x1000/cccccc/0011ff.png",
-//			"https://dummyimage.com/736x1000/cccccc/0011ff.png"
-//		],
-//		"shortDetails": "Sasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem,",
-//		"description": "Lorem versions of Lorem Ipsum.",
-//		"stock": 16,
-//		"new": true,
-//		"sale": true,
 //		"category": "women",
-//		"colors": [
-//			"yellow",
-//			"gray",
-//			"green"
-//		],
-//		"size": [
-//			"M",
-//			"L",
-//			"XL"
-//		],
 //		"tags": [
 //			"nike",
 //			"caprese"
 //		],
-//		"rating": 4,
-//		"variants": [
-//			{
-//				"color": "yellow",
-//				"images": "https://dummyimage.com/736x1000/cccccc/0011ff.png"
-//			},
-//			{
-//				"color": "gray",
-//				"images": "https://dummyimage.com/736x1000/cccccc/0011ff.png"
-//			},
-//			{
-//				"color": "green",
-//				"images": "https://dummyimage.com/736x1000/cccccc/0011ff.png"
-//			}
-//		]
 //	},
+
+
+// {"items":[{"id":183,"title":"Product 1","description":"The md-chips component supports keyboard and screen reader users since Version 1.1.2. In order to achieve this, we modified the chips behavior to select newly appended chips for 300ms before re-focusing the input and allowing the user to type.","brand":"IT Man","model":"Original","manufacturer":"DPQ","avatar":"\/api\/v2\/cms\/contents\/12944\/content","price":1000,"off":157,"categories":[{"id":447,"name":"name"},{"id":449,"name":"test"},{"id":453,"name":"my new category"}],"metafields":[{"id":264,"key":"variant","namespace":"std","product_id":183,"unit":null,"value":"[{\"key\":\"a\",\"image\":\"\/api\/v2\/cms\/contents\/14072\/content\",\"color\":\"blue\",\"size\":\"L\"},{\"image\":\"\/api\/v2\/cms\/contents\/14071\/content\",\"color\":\"yellow\"},{\"image\":\"\/api\/v2\/cms\/contents\/14070\/content\",\"size\":\"S\"}]"},{"id":265,"key":"image","namespace":"theme","product_id":183,"unit":"testd","value":"\/api\/v2\/cms\/contents\/14073\/content"},{"id":267,"key":"color","namespace":"theme","product_id":183,"unit":null,"value":"red"},{"id":268,"key":"size","namespace":"theme","product_id":183,"unit":"mm","value":"M"},{"id":270,"key":"vstore.stock","namespace":"theme","product_id":183,"unit":null,"value":"10"},{"id":271,"key":"vstore.sale","namespace":"theme","product_id":183,"unit":null,"value":"true"},{"id":272,"key":"vstore.new","namespace":"theme","product_id":183,"unit":null,"value":"true"},{"id":273,"key":"rating","namespace":"commiunity","product_id":183,"unit":null,"value":"2"}],"tags":[]}]}
 
 const TIMEOUT = 100
 
@@ -61,8 +23,52 @@ function encodeQueryParams(params) {
 	return Object.keys(params).map(k => `${esc(k)}=${esc(params[k])}`).join('&')
 }
 
+function getProductMetaField(product, key, namespace) {
+	var metas = product.metafields;
+	var found;
+	metas.forEach(meta => {
+		if (meta.key === key && (!namespace || meta.namespace === namespace)) {
+			found = meta;
+		}
+	});
+	if (found) {
+		return found.value;
+	}
+}
+
+function aggregateObjectField(list, key) {
+	var result = [];
+	list.forEach(item => {
+		if (item[key]) {
+			result.push(item[key]);
+		}
+	})
+}
+
 function mapProducts(products) {
-	return [];
+	products.forEach(product => {
+		product.name = product.title;
+		product.shortDetails = product.description;
+		product.salePrice = product.price - (product.off || 0);
+		product.discount = (product.off || 0) * 100 / product.price;
+
+		let variant = JSON.parse(getProductMetaField(product, 'variant', 'theme') || '[]');
+		product.variants = variant;
+		product.puctures = aggregateObjectField(variant, 'image');
+		product.colors = aggregateObjectField(variant, 'color');
+		product.size = aggregateObjectField(variant, 'size');
+		product.stock = getProductMetaField(product, 'vstore.stock', 'theme') || 0;
+		product.new = getProductMetaField(product, 'vstore.new', 'theme') || 0;
+		product.sale = getProductMetaField(product, 'vstore.sale', 'theme') || 0;
+		product.sale = getProductMetaField(product, 'rating', 'community') || 0;
+		product.tags = aggregateObjectField(product.tags, 'name');
+		product.category = aggregateObjectField(product.categories, 'name');
+
+		variant.forEach(item => {
+			item.images = item.image;
+		})
+	});
+	return products;
 }
 
 function getProducts(cb) {
